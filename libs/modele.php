e95dbc0f9395ba9831c848f6cd98f19c20641a12
Lafie-rage <Lafie-rage@github.com>
2020-05-04 18:06:19 +0200

    Adding back void functions

--- libs/modele.php
+++ libs/modele.php
@@ -3,7 +3,7 @@ include_once("maLibSQL.pdo.php");
 include_once "config.php";
 // définit les fonctions SQLSelect, SQLUpdate...
 
-// Utilisateurs ///////////////////////////////////////////////////////////////
+// Users //////////////////////////////////////////////////////////////////////
 
 function register($firstname, $name, $birthday, $mail, $password, $username, $id_role, $id_category) {
   global $dbh;
@@ -19,6 +19,257 @@ function register($firstname, $name, $birthday, $mail, $password, $username, $id
   return SQLInsert($sth);
 }
 
+function getUsers() {
+
+}
+
+function getUser() {
+
+}
+
+function usernameExists() {
+
+}
+
+function validUser() {
+
+}
+
+function updateUser() {
+
+}
+
+function getUsersByRole() {
+
+}
+
+function getUsersByCatAge() {
+
+}
+
+function getUsersByRun() {
+
+}
+
+function getUsersByClub() {
+
+}
+
+function addClub() {
+
+}
+
+function getClub() {
+
+}
+
+function nameExists() {
+
+}
+
+function getClubByUser() {
+
+}
+
+function updateClub() {
+
+}
+
+function addRun() {
+
+}
+
+function delRun() {
+
+}
+
+function getRuns() {
+
+}
+
+function getRun() {
+
+}
+
+function getRunsByCatAge() {
+
+}
+
+function getRunsByUser() {
+
+}
+
+function getRunsByClub() {
+
+}
+
+function getRunsByType_run() {
+
+}
+
+function getRunsByType_field() {
+
+}
+
+function updateRun() {
+
+}
+
+function getCatAge() {
+
+}
+
+function getAllCatAge() {
+
+}
+
+function getCatAgeByUser() {
+
+}
+
+function getCatAgeByRun() {
+
+}
+
+function addParticipateRun() {
+
+}
+
+function delParticipateRun() {
+
+}
+
+function updateParticipateRun() {
+
+}
+
+function getParticipateRun() {
+
+}
+
+function getParticipatesRunByRun() {
+
+}
+
+function getParticipatesRunsByUser() {
+
+}
+
+function addIncludeCharge() {
+
+}
+
+function delIncludeCharge() {
+
+}
+
+function updateIncludeCharge() {
+
+}
+
+function getIncludeCharges() {
+
+}
+
+function getIncludeCharge() {
+
+}
+
+function getIncludeChargeByRun() {
+
+}
+
+function getIncludeChargeByTypeCost() {
+
+}
+
+function addTypeCost() {
+
+}
+
+function delTypeCost() {
+
+}
+
+function getTypeCost() {
+
+}
+
+function getAllTypeCosts() {
+
+}
+
+function addTypeField() {
+
+}
+
+function delTypeField() {
+
+}
+
+function getTypeField() {
+
+}
+
+function getAllTypesField() {
+
+}
+
+function getTypeFieldByRun() {
+
+}
+
+function addTypeRun() {
+
+}
+
+function delTypeRun() {
+
+}
+
+function updateTypeRun() {
+
+}
+
+function getTypeRun() {
+
+}
+
+function getAllTypesRun() {
+
+}
+
+function getTypeRunByRun() {
+
+}
+
+function addRegistered_run() {
+
+}
+
+function delRegistered_run() {
+
+}
+
+function getRegistered_run() {
+
+}
+
+function getAllRegistered_runs() {
+
+}
+
+function getRegistered_runByRun() {
+
+}
+
+function getRegistered_runByUser() {
+
+}
+
+
+
+ // Exemple de fonctions... (Ancien fonctionnement, regardez la fonction register pour voir la nouvelle synthaxe...)
 function getUser($id) {
   $sql = "SELECT id, pseudo, color, isAdmin FROM USERS WHERE id=$id";
   return parcoursRs(SQLSelect($sql));
