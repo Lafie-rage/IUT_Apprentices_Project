9298b6a1a4d65e87176c89b91f5aa1bb59916d04
Lafie-rage <Lafie-rage@github.com>
2020-05-12 18:01:41 +0200

    Updating apropos' page headers

--- apropos.php
+++ apropos.php
@@ -1,50 +1,121 @@
+<style media="screen">
+
+  /* header's style */
+  /* CSS NavBar */
+  .navbar {background:url(ressources/header_nav_herbe.png), url(ressources/header_nav_ciel.jpg); position:absolute; background-position:bottom; background-size:40%, cover; background-repeat:repeat-x; left:0;right:0;top:0; margin-bottom: 2%;}
+  .slogan {color: #0275b4 !important; font-family: "The Blacklist" !important;}
+  #navbarNav {background-color: #c5bfbf; line-height: 2vw; width:40%; position: absolute; bottom: 0; right: 30%;}
+  #header-img-profil {position: relative; left: 90;}
+  .navbar.navbar-dark .navbar-toggler {color: #0275b4 !important; }
+  .nav-link {color: #0275b4 !important; text-transform: uppercase; font-weight: 700;}
+  .nav-link:hover {color: #F8F9FA;}
+  .fas {color: #ffffff;}
+
+  /* Reponsive for laptot */
+  @media (min-width: 1025px) and (max-width: 1280px) {
+
+    /* Responsive NavBar */
+    .col-slogan {display: inline-block;}
+    .slogan {position: relative; left: 0rem; top: 2rem; font-size: 2.3rem !important;}
+    .navbar {padding: 0 !important; }
+    #navbarNav {position: absolute !important; right: 18%; width: 60%;}
+    .col-header-profil {display: block; position: relative; bottom: 0rem; left: 0rem;}
+  }
+
+  /* Responsive for tablet */
+  @media (min-width: 768px) and (max-width: 1024px) {
+
+    /* Responsive NavBar */
+    .col-slogan {display: inline-block;}
+    .slogan {position: relative; left: 13rem; top: 9rem;}
+    .navbar {padding: 0 !important; }
+    #navbarNav {position: initial;}
+    .col-header-profil {display: block; position: relative; bottom: 0rem; left: 0rem;}
+
+  }
+
+  /* Responsive for tablet landscape */
+  @media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+
+    /* Responsive NavBar */
+    .col-slogan {display: inline-block;}
+    .slogan {position: relative; left: 0rem; top: 2rem; font-size: 2.3rem !important;}
+    .navbar {padding: 0 !important; }
+    #navbarNav {position: absolute !important; right: 18%; width: 60%;}
+    .col-header-profil {display: block; position: relative; bottom: 0rem; left: 0rem;}
+
+  }
+
+  /* Responsive for mobile */
+  @media (min-width: 330px) and (max-width: 768px) {
+
+    /* Responsive NavBar */
+    .navbar {padding: 0 !important;}
+    .col-slogan {display: none;}
+    #navbarNav {position: relative; right: 0%; width: 100%;}
+    .navbarNav .nav-item {margin: 0rem !important;}
+    .col-header-profil {display: block; position: relative !important; right: 7rem; top: 2rem;}
+    .col-header-profil .row-sign {display: none;}
+
+    .mx-3, .ml-3 {margin-left: 0rem !important}
+
+  }
+
+
+  .thumbnail{
+      background-color: #c5bfbf;
+      border-radius: 10px;
+      text-align: center;
+      font-weight: bold;
+      border:2px solid black;
+      margin-bottom: 10px;
+  }
+
+  #par {
+      margin-right: -50%;
+  }
+
+  body {
+      background-image: url(../ressources/arriereplan.png);
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-attachment: fixed;
+  }
+
+</style>
+
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Can'APP</title>
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+  <link rel="icon" type="image/png" href="ressources/logo_site.png">
+    <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- Google Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
   <!-- Bootstrap core CSS -->
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
   <!-- Material Design Bootstrap -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/css/mdb.min.css" rel="stylesheet">
-  <!-- Your custom styles (optional) -->
-<link href="css/apropos.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/css/mdb.min.css" rel="stylesheet">
+
+  <!-- JQuery -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/js/mdb.min.js"></script>
+
 </head>
 
 <body>
   <!-- DEBUT DU PROJET-->
 
-  <img id="logo" src="https://cdn.discordapp.com/attachments/691594199048650803/704348261850415165/logo-canapp.png"
-    alt="">
-
-  <div class="sky">
-    <div class="container-fluid" id="onNav">
-
-    </div>
-  </div>
-
-  <ul class="container navIndex">
-    <li>
-      <a class="text-uppercase" id="accueil"><i class="fas fa-running"></i>&nbsp; Courses</a>
-    </li>
-    <li>
-      <a class="text-uppercase" id="resultats"><i class="fas fa-poll"></i>&nbsp; Résultats</a>
-    </li>
-    <li>
-      <a class="text-uppercase" id="depenses"><i class="fas fa-dollar-sign"></i>&nbsp; Dépenses</a>
-    </li>
-    <li>
-      <a class="text-uppercase" id="a-propos"><i class="fas fa-home"></i>&nbsp; À propos</a>
-    </li>
-  </ul>
-
-
-
+  <?php include_once "header.php"; ?>
   <div class="container mt-5">
     <div class="row">
       <div class="col-md-8 p-0">
