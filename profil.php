c1e76706a0768bf8aa7f75fa1b1be2f97a2bc932
Lafie-rage <Lafie-rage@github.com>
2020-05-12 17:46:29 +0200

    Final profil page

--- profil.php
+++ profil.php
@@ -4,9 +4,65 @@
 body,html{margin:0}
 body{background-image:url(ressources/body-background.jpg);background-size:cover;background-repeat:no-repeat;background-attachments:fixed;overflow-x:hidden}
 
-main {
-  top : 200px;
-  position: absolute;
+/* header's style */
+/* CSS NavBar */
+.navbar {background:url(ressources/header_nav_herbe.png), url(ressources/header_nav_ciel.jpg); position:absolute; background-position:bottom; background-size:40%, cover; background-repeat:repeat-x; left:0;right:0;top:0; margin-bottom: 2%;}
+.slogan {color: #0275b4 !important; font-family: "The Blacklist" !important;}
+#navbarNav {background-color: #c5bfbf; line-height: 2vw; width:40%; position: absolute; bottom: 0; right: 30%;}
+#header-img-profil {position: relative; left: 90;}
+.navbar.navbar-dark .navbar-toggler {color: #0275b4 !important; }
+.nav-link {color: #0275b4 !important; text-transform: uppercase; font-weight: 700;}
+.nav-link:hover {color: #F8F9FA;}
+.fas {color: #ffffff;}
+
+/* Reponsive for laptot */
+@media (min-width: 1025px) and (max-width: 1280px) {
+
+  /* Responsive NavBar */
+  .col-slogan {display: inline-block;}
+  .slogan {position: relative; left: 0rem; top: 2rem; font-size: 2.3rem !important;}
+  .navbar {padding: 0 !important; }
+  #navbarNav {position: absolute !important; right: 18%; width: 60%;}
+  .col-header-profil {display: block; position: relative; bottom: 0rem; left: 0rem;}
+}
+
+/* Responsive for tablet */
+@media (min-width: 768px) and (max-width: 1024px) {
+
+  /* Responsive NavBar */
+  .col-slogan {display: inline-block;}
+  .slogan {position: relative; left: 13rem; top: 9rem;}
+  .navbar {padding: 0 !important; }
+  #navbarNav {position: initial;}
+  .col-header-profil {display: block; position: relative; bottom: 0rem; left: 0rem;}
+
+}
+
+/* Responsive for tablet landscape */
+@media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+
+  /* Responsive NavBar */
+  .col-slogan {display: inline-block;}
+  .slogan {position: relative; left: 0rem; top: 2rem; font-size: 2.3rem !important;}
+  .navbar {padding: 0 !important; }
+  #navbarNav {position: absolute !important; right: 18%; width: 60%;}
+  .col-header-profil {display: block; position: relative; bottom: 0rem; left: 0rem;}
+
+}
+
+/* Responsive for mobile */
+@media (min-width: 330px) and (max-width: 768px) {
+
+  /* Responsive NavBar */
+  .navbar {padding: 0 !important;}
+  .col-slogan {display: none;}
+  #navbarNav {position: relative; right: 0%; width: 100%;}
+  .navbarNav .nav-item {margin: 0rem !important;}
+  .col-header-profil {display: block; position: relative !important; right: 7rem; top: 2rem;}
+  .col-header-profil .row-sign {display: none;}
+
+  .mx-3, .ml-3 {margin-left: 0rem !important}
+
 }
 
 /* CSS Connexion profil */
