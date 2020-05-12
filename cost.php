7d546d354172acb989d92e4850a8205def86e03d
Lafie-rage <Lafie-rage@github.com>
2020-05-12 18:01:57 +0200

    Adding new pages that will have to be built

---
+++ cost.php
@@ -0,0 +1,97 @@
+<style media="screen">
+
+  /* header's style */
+  /* CSS NavBar */
+  .navbar {background:url(ressources/header_nav_herbe.png), url(ressources/header_nav_ciel.jpg); position:absolute; background-position:bottom; background-size:40%, cover; background-repeat:repeat-x; left:0;right:0;top:0; margin-bottom: 2%;}
+  .slogan {color: #0275b4 !important; font-family: "The Blacklist" !important;}
+  #navbarNav {background-color: #c5bfbf; line-height: 2vw; width:40%; position: absolute; bottom: 0; right: 30%;}
+  #header-img-profil {position: relative; left: 90;}
+  .navbar.navbar-dark .navbar-toggler {color: #0275b4 !important; }
+  .nav-link {color: #0275b4 !important; text-transform: uppercase; font-weight: 700;}
+  .nav-link:hover {color: #F8F9FA;}
+  .fas {color: #ffffff;}
+
+  /* Reponsive for laptot */
+  @media (min-width: 1025px) and (max-width: 1280px) {
+
+    /* Responsive NavBar */
+    .col-slogan {display: inline-block;}
+    .slogan {position: relative; left: 0rem; top: 2rem; font-size: 2.3rem !important;}
+    .navbar {padding: 0 !important; }
+    #navbarNav {position: absolute !important; right: 18%; width: 60%;}
+    .col-header-profil {display: block; position: relative; bottom: 0rem; left: 0rem;}
+  }
+
+  /* Responsive for tablet */
+  @media (min-width: 768px) and (max-width: 1024px) {
+
+    /* Responsive NavBar */
+    .col-slogan {display: inline-block;}
+    .slogan {position: relative; left: 13rem; top: 9rem;}
+    .navbar {padding: 0 !important; }
+    #navbarNav {position: initial;}
+    .col-header-profil {display: block; position: relative; bottom: 0rem; left: 0rem;}
+
+  }
+
+  /* Responsive for tablet landscape */
+  @media (min-width: 768px) and (max-width: 1024px) and (orientation: landscape) {
+
+    /* Responsive NavBar */
+    .col-slogan {display: inline-block;}
+    .slogan {position: relative; left: 0rem; top: 2rem; font-size: 2.3rem !important;}
+    .navbar {padding: 0 !important; }
+    #navbarNav {position: absolute !important; right: 18%; width: 60%;}
+    .col-header-profil {display: block; position: relative; bottom: 0rem; left: 0rem;}
+
+  }
+
+  /* Responsive for mobile */
+  @media (min-width: 330px) and (max-width: 768px) {
+
+    /* Responsive NavBar */
+    .navbar {padding: 0 !important;}
+    .col-slogan {display: none;}
+    #navbarNav {position: relative; right: 0%; width: 100%;}
+    .navbarNav .nav-item {margin: 0rem !important;}
+    .col-header-profil {display: block; position: relative !important; right: 7rem; top: 2rem;}
+    .col-header-profil .row-sign {display: none;}
+
+    .mx-3, .ml-3 {margin-left: 0rem !important}
+
+  }
+</style>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Can'APP</title>
+  <link rel="icon" type="image/png" href="ressources/logo_site.png">
+    <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- Google Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+  <!-- Bootstrap core CSS -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/css/mdb.min.css" rel="stylesheet">
+
+  <!-- JQuery -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/js/mdb.min.js"></script>
+
+</head>
+  <body>
+    <?php include_once "header.php"; ?>
+
+    <h1> PAGE EN CONSTRUCTION... </h1>
+  </body>
+</html>
